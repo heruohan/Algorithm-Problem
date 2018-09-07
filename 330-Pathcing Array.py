e779ca648fc1a2ed87a48e1338b145029ec58417
薛定谔的猫 <465151160@qq.com>
2018-09-07 17:06:50 +0800

    Update 330-Pathcing Array.py

--- 330-Pathcing Array.py
+++ 330-Pathcing Array.py
@@ -4,6 +4,26 @@ Created on Fri Sep  7 02:15:41 2018
 
 @author: hecongcong
 """
+'''
+The Problem 330:Patchinig Array
+Given a sorted positive integer array nums and an integer n,
+add/patch elements to the array such that any number in range [1, n] inclusive
+can be formed by the sum of some elements in the array.
+Return the minimum number of patches required.
+
+Example:
+Input:nums = [1,5,10],n=20
+Output:2
+Explanation:The two patches can be[2,4].
+'''
+'''
+The answer: 可以初始化res=1,其表示第一个不能被表示的数，比如res=5,表示能表示[0,5)，
+既0,1,2,3,4,从5开始不能表示，然后不断更新res,直至满足res>n为止.
+第一步：初始化res=1,计数器count=0.
+
+第二步：当数组元素循环完，或者res<nums[i]时，加入元素res，更新res和count；否则
+用nums[i]更新res,指针前移一位；直至res>n.
+'''
 
 class Solution:
     def minPatches(self,nums,n):
@@ -14,16 +34,13 @@ class Solution:
         while(True):
             if(res>n):
                 return(count)
-            if(i<lens):
-                if(res<nums[i]):
-                    res+=res
-                    count+=1
-                else:
-                    res+=nums[i]
-                    i+=1
-            else:
+            if(i>=lens or res<nums[i]):
                 res+=res
                 count+=1
+            else:
+                res+=nums[i]
+                i+=1
+                
                 
                 
                 
