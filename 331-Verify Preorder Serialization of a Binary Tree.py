8bcfee08df41735be04a8f71c62d5cc4080051de
薛定谔的猫 <465151160@qq.com>
2018-09-14 11:10:27 +0800

    Update 331-Verify Preorder Serialization of a Binary Tree.py

--- 331-Verify Preorder Serialization of a Binary Tree.py
+++ 331-Verify Preorder Serialization of a Binary Tree.py
@@ -4,6 +4,27 @@ Created on Thu Sep 13 23:55:54 2018
 
 @author: hecongcong
 """
+'''
+The Problem 331:Verify Preorder Serialization of a binary tree.
+    One way to serialize a binary tree is to use pre-order traversal. 
+When we encounter a non-null node, we record the node's value.
+If it is a null node, we record using a sentinel value such as #.
+For example, the above binary tree can be serialized to the  string
+"9,3,4,#,#,1,#,#,2,#,6,#,#", where # represents a null node.
+    Given a string of comma separated values, verify whether it is a correct
+preorder traversal serialization of a binary tree. Find an algorithm without
+reconstructing the tree.
+'''
+
+'''
+The answer:本题是确认一个二叉树前序字符串是否正确，依据题意，当字符串中连续出现两个#号时，
+表示这个node为叶节点，可以将此叶节点转换为#号，例如'4##'可以转换为'#'，当字符串循环完后，如果只
+剩下'#'，则为正确.
+思路：
+第一步：将preorder转换为字符形式的list.
+第二步：创建一个栈s，依次进入preorder中的元素，当遇到连续两个#时，比如'4##'变为'#'，直至最终
+栈中的元素只剩下为'#'.
+'''
 
 class Solution:
     def isValidSerialization(self,preorder):
