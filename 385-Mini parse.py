bb647567e35278f5176618d99ca825b1221e2f6c
薛定谔的猫 <465151160@qq.com>
2019-01-22 14:00:58 +0800

    Update 385-Mini parse.py

--- 385-Mini parse.py
+++ 385-Mini parse.py
@@ -5,8 +5,79 @@ Created on Mon Jan 21 23:48:47 2019
 @author: hecongcong
 """
 
+'''
+The problem 385:Mini parse
+Given a nested list of integers representing as a string,implement a parse to deserialize it.
+each element is either an integer,or a list--whose elements may also be integers or other lists.
+Note:you may assume that the string is well-formed:
+1.string is non-empty.
+2.string does contain white-spaces.
+3.string only contain digits 0-9, [, - ,, ].
 
-#解法1
+Example1:
+Given s='324'
+you should return a NestedInteger object which contains a single integer 324.
+Example2:
+given s='[123,[456,[789]]]",
+return a NestedInteger object containing a nested list with 2 elements:
+1.an integer containing value 123.
+2.a nested list containing two element:
+     i:an integer containing value 456.
+     ii:a nested list with one element:
+          (1)a integer containing value 789.
+'''
+
+
+'''
+this is the interface that allows for creating nested lists.
+you should not implement it,or speculate about its implementation.
+''''
+
+#class NestedInteger:
+#    def __init__(self,val):
+#        '''
+#        if val is not specified,initializes an empty list.
+#        otherwise initializes a single integer equal to val.
+#        '''
+#    def isIntegers(self):
+#        '''
+#        return true if the nestedinteger holds a single integer,ranther than a nested list.
+#        :rtype:bool
+#        '''
+#    def add(self,elem):
+#        '''
+#        set this nestedinteger to hold a nested list,and adds a nested integer elem to it.
+#        :rtype:void
+#        '''
+#    def setInteger(self,value):
+#        '''
+#        set this nestedinteger to hold a single integer equal to value.
+#        :rtype:void
+#        '''
+#    def getInteger(self):
+#        '''
+#        return the single integer that this nestedinteger holds,if it holds a single integer.
+#        return None if this nestedinteger holds a nested list.
+#        rtype:int
+#        '''
+#    def getList(self):
+#        '''
+#        return the nested list that this nestedinteger holds,if it holds a nested list.
+#        return None if this nestedinteger holds a single integer.
+#        :rtypr:List[nestedinteger]
+#        '''
+
+
+'''
+The answer:本题是给定一个字符串，实现一个解析器解析它.本题有两种解法，一种方法是递归，另一种方法是
+迭代辅助栈数据结构.
+
+解法1：迭代+栈
+1.首先构建栈st和start,start是记录起始位置的索引，然后循环字符串s；
+2.当s[i]为'['时，初始化一个NestedInteger对象，并将其压入栈中，同时将start更新为i+1;
+'''
+        
+#解法1:迭代+栈
 class Solution:
     def deserialize(self,s):
         if(len(s)==0):
@@ -30,7 +101,7 @@ class Solution:
         return(st[-1])
         
         
-#解法2
+#解法2：递归
 class Solution:
     def deserialize(self,s):
         if(len(s)==0):
