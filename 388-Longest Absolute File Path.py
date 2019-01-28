e91c2c106c6a4c15fa8b0e0e5741581c4bc9b4e5
薛定谔的猫 <465151160@qq.com>
2019-01-28 17:20:32 +0800

    Update 388-Longest Absolute File Path.py

--- 388-Longest Absolute File Path.py
+++ 388-Longest Absolute File Path.py
@@ -5,6 +5,46 @@ Created on Fri Jan 25 23:45:22 2019
 @author: hecongcong
 """
 
+'''
+The problem 388:Longest absolute file path
+Suppose we abstract our file system by a string in the following manner:
+the string "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext" represents:
+dir
+    subdir1
+    subdir2
+        file.ext
+The directory dir contains an empty sub-directory subdir1 and a sub-directory subdir2
+containing a file file.ext.
+The string "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+represents:
+dir
+    subdir1
+        file1.ext
+        subsubdir1
+    subdir2
+        subsubdir2
+            file2.ext
+The directory dir contains two sub-directories subdir1 and subdir2.subdir1 contains a file file1.ext and 
+an empty second-level sub-directory subsubdir1.subdirs contains a second-level subsubdir2 containing a file
+file2.ext.
+we are interested in finding the longest(number of characters)absolute path to a file within our file system.
+For example,in the second above example,the longest absolute path is "dir/subdir2/subsubdir2/file2.ext",and its 
+length is 32(not including the double quotes).
+Given a string representing the file system in the above format,return the length of the longest absolute path to 
+file in the abstracted file system.if there is no file in the system,return 0.
+
+Note:
+1.The name of file contains at least a '.' and an extension.
+2.The name of a directory or sub-directory will not contain a '.'.
+Time complexity required:O(n) where n is the size of the input string.
+Notice that a/aa/aaa/file1.txt is not the longest file path,if there is another path aaaaaaaaaaaaaaaaaaaaa/sth.png.
+'''
+
+
+'''
+The answer:本题的题意是给定一个代表文件系统的字符串，返回其中最长绝对路径的长度，没有文件的话就返回0.
+本题可用三种思路解答.
+'''
 #解法1
 class Solution:
     def lengthLongestPath(self,input):
