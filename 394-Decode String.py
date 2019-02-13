671cd6f6a6631ffc039351469039addbf54c8cc9
薛定谔的猫 <465151160@qq.com>
2019-02-13 15:00:07 +0800

    Update 394-Decode String.py

--- 394-Decode String.py
+++ 394-Decode String.py
@@ -5,7 +5,20 @@ Created on Tue Feb 12 22:06:11 2019
 @author: hecongcong
 """
 
+'''
+The problem 394:Decode String
+Given an encoded string,return it is decoded string.
+The encoding rule is:k[encoded_string],where the encoded_string inside the square brackets is being repeated exactly k times.
+Note that k is guaranteed to be a positive integer.
+you may assume that the input string is always valid:No extra white spaces,square brackets are well-formed,etc.
+furthermore,you may assume that the original data does not contain any digits and that digits are only for those repeat numbers,
+k.For example,there will not be input like 3a or 2[4].
 
+Examples:
+s="3[a]2[bc]",return "aaabcbc".
+s= "3[a2[c]]",return "accaccacc".
+s= "2[abc]3[cd]ef",return "abcabccdcdcdef".
+'''
 
 
 #解法1:迭代+栈
